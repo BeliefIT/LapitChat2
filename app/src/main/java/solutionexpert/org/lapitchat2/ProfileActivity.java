51191babbbcba0424dfa370b1d56c1a0352bd0f3
Sachin Kr Pandey <developersachinkr@gmail.com>
2018-02-05 16:11:56 +0530

    finishing user profile

--- app/src/main/java/solutionexpert/org/lapitchat2/ProfileActivity.java
+++ app/src/main/java/solutionexpert/org/lapitchat2/ProfileActivity.java
@@ -1,12 +1,28 @@
 package solutionexpert.org.lapitchat2;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.squareup.picasso.Picasso;
 
 public class ProfileActivity extends AppCompatActivity {
 
-    TextView mDisplayUserID;
+    ImageView mProfileImage;
+    TextView mProfileName, mProfileStatus, mProfileFriendsCount;
+    Button mProfileSendReqBtn;
+
+    private DatabaseReference mUserDatabase;
+
+    private ProgressDialog progressDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,7 +30,41 @@ public class ProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile);
 
         String profile_userID = getIntent().getStringExtra("user_id");
-        mDisplayUserID = findViewById(R.id.profile_DisplayName);
-        mDisplayUserID.setText(profile_userID);
+
+        mUserDatabase = FirebaseDatabase.getInstance().getReference().child("user").child(profile_userID);
+
+        mProfileImage = findViewById(R.id.profile_image);
+        mProfileName = findViewById(R.id.profile_displayName);
+        mProfileStatus = findViewById(R.id.profile_status);
+        mProfileFriendsCount = findViewById(R.id.profile_totalFriends);
+        mProfileSendReqBtn = findViewById(R.id.profile_send_req_btn);
+
+        progressDialog = new ProgressDialog(this);
+        progressDialog.setTitle("Loading User Data");
+        progressDialog.setMessage("Please Wait while we load the User data");
+        progressDialog.setCanceledOnTouchOutside(false);
+        progressDialog.show();
+
+        mUserDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                String display_name = dataSnapshot.child("name").getValue().toString();
+                String status = dataSnapshot.child("status").getValue().toString();
+                String image = dataSnapshot.child("image").getValue().toString();
+
+                mProfileName.setText(display_name);
+                mProfileStatus.setText(status);
+                Picasso.with(ProfileActivity.this).load(image).placeholder(R.drawable.default_avatar).into(mProfileImage);
+                progressDialog.dismiss();
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+
     }
 }
